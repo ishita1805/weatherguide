2ed692998fa429638fa3a4cb358b4b5e5a993cc4
Ishita Kabra <58788445+ishita1805@users.noreply.github.com>
2020-08-26 10:58:17 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode =(address, callback)=>{
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoiaXNoaXRha2FicmEiLCJhIjoiY2s0NGtmeWpsMGZxZTNsbzZwamllZTR0ZCJ9.u58dn25ppPCKMisfHn6LBA&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=Your API key goes here'
     request({url, json: true }, (error , response ,body)=>{
       if(error){
         callback('unable to connect to the internet')
